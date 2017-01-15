2f486a7c7be682a9f93b1d13f3924ea5fa1099ff
Brian Abelson <brianabelson@gmail.com>
2017-01-15 01:06:36 -0500

    fix tests

--- tests/test_pipeline.sh
+++ tests/test_pipeline.sh
@@ -1,7 +1,3 @@
 #!/bin/sh 
 
-bnpl file.directory --path='fixtures/' |\
-	bnpl taglib.get_tags |\
-	bnpl fpcalc.uid |\
-	bnpl core.importer |\ 
-	jq .
+bnpl file.directory --path='fixtures/' | bnpl taglib.get_tags | bnpl fpcalc.uid | bnpl core.importer | jq .
